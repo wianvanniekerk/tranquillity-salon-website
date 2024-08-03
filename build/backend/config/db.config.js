c2858f918c425cedebb9c2305c6b9b467f020907
wianvanniekerk <wianvanniekerk@protonmail.com>
2024-08-03 08:06:54 +0200

    db port update

--- build/backend/config/db.config.js
+++ build/backend/config/db.config.js
@@ -7,7 +7,8 @@ const config = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 config.connect((err) => {

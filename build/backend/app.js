cc3e2f326612ada0887893582f9c238d1ba6ec20
wianvanniekerk <wianvanniekerk@protonmail.com>
2024-08-07 14:14:23 +0200

    db config changes

--- build/backend/app.js
+++ build/backend/app.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const flash = require('connect-flash');
+const dbConfig = require('./config/db.config');
+
+// Import route handlers
 const authRoutes = require('./routes/auth.routes');
 const indexRoutes = require('./routes/index.routes');
 const userProfilesRoutes = require('./routes/userProfiles.routes');
@@ -8,18 +15,14 @@ const productsRoutes = require('./routes/products.routes');
 const inquiriesRoutes = require('./routes/inquiries.routes');
 const mailRoutes = require('./routes/mail.routes');
 const formsRoutes = require('./routes/forms.routes');
-const path = require('path');
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-const flash = require('connect-flash');
-const dbConfig = require('./config/db.config');
 
+// Set up views and static files
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../../build/frontend/views'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../../build')));
 
+// Set up session store
 const sessionStore = new MySQLStore({
     expiration: 86400000,
     createDatabaseTable: true,
@@ -31,7 +34,7 @@ const sessionStore = new MySQLStore({
             data: 'data'
         }
     }
-}, dbConfig);
+}, dbConfig.config);
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -46,14 +49,17 @@ app.use(session({
 
 app.use(flash());
 
+// Set up middleware for flash messages
 app.use((req, res, next) => {
     res.locals.success_messages = req.flash('success');
     res.locals.error_messages = req.flash('error');
     next();
 });
 
+// Set up JSON parsing
 app.use(express.json());
 
+// Use route handlers
 app.use('/', indexRoutes);
 app.use(authRoutes);
 app.use('/user', userProfilesRoutes);
--- build/backend/config/db.config.js
+++ build/backend/config/db.config.js
@@ -1,45 +1,40 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
 const mysql = require('mysql');
 
-const config = mysql.createConnection({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   multipleStatements: true 
-});
+};
 
-config.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to database.');
-});
+let connection;
 
 function handleDisconnect() {
   connection = mysql.createConnection(dbConfig);
 
   connection.connect(err => {
-      if (err) {
-          console.error('Error connecting to the database:', err);
-          setTimeout(handleDisconnect, 2000);
-      } else {
-          console.log('Connected to the database.');
-      }
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log('Connected to the database.');
+    }
   });
 
   connection.on('error', err => {
-      console.error('Database error:', err);
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-          handleDisconnect();
-      } else {
-          throw err;
-      }
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
   });
 }
 
-module.exports = config;
+handleDisconnect();
+
+module.exports = { config: connection, handleDisconnect };
